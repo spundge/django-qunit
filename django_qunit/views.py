01b4327d9a0fb809ccff6601656b8c566600c3f0
Julien St-Laurent <julien.stlaurent@gmail.com>
2012-01-23 11:44:07 -0500

    Load root suite.json as default configuration that can be overriden by suite specific configuration.

--- django_qunit/views.py
+++ django_qunit/views.py
@@ -4,7 +4,16 @@ from django.utils import simplejson
 
 import os
 
-def get_suite_context(request, path):
+def load_configuration(path):
+    try:
+        file = open(os.path.join(path, 'suite.json'), 'r')
+        json = file.read()
+        return simplejson.loads(json)
+    except:
+        return {}
+
+
+def get_suite_context(path):
     full_path = os.path.join(settings.QUNIT_TEST_DIRECTORY, path)
     full_path, directories, files = os.walk(full_path).next()
 
@@ -22,26 +31,28 @@ def get_suite_context(request, path):
     suite['absolute_urls'] = []
     suite['static_urls'] = []
 
-    # load suite.json if present
-    if 'suite.json' in files:
-        file = open(os.path.join(full_path, 'suite.json'), 'r')
-        json = file.read()
-        suite.update(simplejson.loads(json))
+    # Load base suite configuration
+    suite.update(load_configuration(settings.QUNIT_TEST_DIRECTORY))
+
+    # load suite configuration
+    suite.update(load_configuration(full_path))
 
     previous_directory = parent_directory(path)
 
     return {
         'files': [path + file for file in files if file.endswith('js')],
-        'in_subdirectory': True and (previous_directory is not None) or False,
+        'in_subdirectory': previous_directory is not None,
         'previous_directory': previous_directory,
-        'qunit_url' : settings.QUNIT_URL,
+        'qunit_url': settings.QUNIT_URL,
         'subsuites': directories,
         'suite': suite,
     }
 
-def run_tests(request, path, template_name = 'qunit/index.html'):
-    suite_context = get_suite_context(request, path)
+
+def run_tests(request, path, template_name='qunit/index.html'):
+    suite_context = get_suite_context(path)
     return render(request, template_name, suite_context)
+
 
 def parent_directory(path):
     """
